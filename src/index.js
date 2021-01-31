ca7514de34b1f03feb89211925d1726ee3f1851a
Irina Komarova <toffee@ukr.net>
2021-01-31 18:19:34 +0200

    init: task typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+  let minnummber;
+  if ((!array) || (array.length === 0)) return 0;
+  minnummber = array[0];
+  for (var i = 1; i < array.length; i++) {
+    if (array[i] < minnummber) { minnummber = array[i]; }
+  }
+  return (minnummber);
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+  let maxnummber;
+  if ((!array) || (array.length === 0)) return 0;
+  maxnummber = array[0];
+  for (var i = 1; i < array.length; i++) {
+    if (array[i] > maxnummber) { maxnummber = array[i]; }
+  }
+  return (maxnummber);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+  let avgnumber = 0;
+  if ((!array) || (array.length === 0)) return 0;
+  for (var i = 0; i < array.length; i++) {
+    avgnumber = avgnumber + array[i];
+  }
+  avgnumber = avgnumber / array.length;
+
+  return (avgnumber);
 }
